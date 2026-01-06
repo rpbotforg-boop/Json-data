dba798499c889ddb3b73369f914bd84f1440e440
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-07 00:44:37 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os, requests
+from pyrogram import Client, filters
+from security import is_admin
+
+API = "http://YOUR_SERVER/search/mobile/"
+HEAD = {"x-api-key": os.getenv("API_KEY")}
+
+app = Client(
+    "trackerbot",
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    bot_token=os.getenv("BOT_TOKEN")
+)
+
+@app.on_message(filters.command("mobile"))
+async def mobile_search(client, message):
+    if not is_admin(message.from_user.id):
+        return await message.reply("❌ Unauthorized")
+
+    if len(message.command) < 2:
+        return await message.reply("Use: /mobile 8601513360")
+
+    num = message.command[1]
+    res = requests.get(API + num, headers=HEAD).json()
+    await message.reply(f"<pre>{res}</pre>")
+
+app.run()
